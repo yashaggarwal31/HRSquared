7180e11665608dead0ab1e1a2faf73b7480703e0
Mahammad Siraj. Cheruvu <mscheruvu@EX2INDIA.LOCAL>
2024-05-03 16:01:57 +0530

    added user check in db

---
+++ src/lib/users.ts
@@ -0,0 +1,25 @@
+import { notFound } from "next/navigation";
+import { dbConnect } from "./renderDB";
+import { Database } from "@/types/database.types";
+
+export async function getUserFromDb(email, password) {
+    const query = {
+        text: 
+        `select u.id, u.username, u.email, r.rolename
+        from users u
+        left join userrole_mapping um
+        on u.id = um.user_id
+        left join roles r
+        on um.role_id = r.id
+        where u.email=$1 and password=$2;`,
+        values: [email, password],
+    }
+
+    const client = await dbConnect();
+    const data = await client.query(query);
+    if(data.rowCount>0) {
+        return data.rows;
+    }else {
+        notFound();
+    }
+}
