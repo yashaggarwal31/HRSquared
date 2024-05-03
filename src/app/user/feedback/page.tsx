42dc82dac60e0b557ed148a435edde7dbca72097
Dheeraj Gupta <dhgupta@EX2INDIA.LOCAL>
2024-05-03 16:27:02 +0530

    modified feedbacks

--- src/app/user/feedback/page.tsx
+++ src/app/user/feedback/page.tsx
@@ -3,49 +3,49 @@ import React, { Suspense, use, useState } from "react";
 import { addFeedback } from "@/lib/feedbacks";
 import Feedbacks from "./feedbackForm";
 
-export const handleSubmit = async (feedbackDto) => {
-  console.log("hello dheeraj");
-  const res = await addFeedback(feedbackDto);
-  console.log(res);
-  // console.log("run");
-  // if (title.trim() === "") {
-  //   setAck("Title is empty");
-  //   setConformation(false);
-  //   return;
-  // }
-  // if (feedback.trim() === "") {
-  //   setAck("Feedback is empty");
-  //   setConformation(false);
-  //   return;
-  // }
-  // if (!conformation && !isAnonymous) {
-  //   setShowDialog(true);
-  //   return;
-  // }
-  // const feedbackDTO = {
-  //   title: title,
-  //   feedback: feedback,
-  //   anonymous: isAnonymous,
-  // };
-  // setSending(true);
-  // const url = "/api/feedbacks/addfeedback";
-  // try {
-  //   // const feedbackResponse = await addFeedback(feedbackDTO);
+// export const handleSubmit = async (feedbackDto) => {
+//   console.log("hello dheeraj");
+//   const res = await ;
+//   console.log(res);
+//   // console.log("run");
+//   // if (title.trim() === "") {
+//   //   setAck("Title is empty");
+//   //   setConformation(false);
+//   //   return;
+//   // }
+//   // if (feedback.trim() === "") {
+//   //   setAck("Feedback is empty");
+//   //   setConformation(false);
+//   //   return;
+//   // }
+//   // if (!conformation && !isAnonymous) {
+//   //   setShowDialog(true);
+//   //   return;
+//   // }
+//   // const feedbackDTO = {
+//   //   title: title,
+//   //   feedback: feedback,
+//   //   anonymous: isAnonymous,
+//   // };
+//   // setSending(true);
+//   // const url = "/api/feedbacks/addfeedback";
+//   // try {
+//   //   // const feedbackResponse = await addFeedback(feedbackDTO);
 
-  //   setAck("Feedback submitted successfully");
-  //   setAckClass("text-green-700 font-semibold pt-1");
-  //   setTitle("");
-  //   setFeedback("");
-  //   setIsAnonymous(false);
-  //   setConformation(false);
-  //   setSending(false);
-  // } catch (error) {
-  //   setAck("Error while submitting feedback");
-  //   setConformation(false);
-  //   setAck("");
-  //   setSending(false);
-  // }
-};
+//   //   setAck("Feedback submitted successfully");
+//   //   setAckClass("text-green-700 font-semibold pt-1");
+//   //   setTitle("");
+//   //   setFeedback("");
+//   //   setIsAnonymous(false);
+//   //   setConformation(false);
+//   //   setSending(false);
+//   // } catch (error) {
+//   //   setAck("Error while submitting feedback");
+//   //   setConformation(false);
+//   //   setAck("");
+//   //   setSending(false);
+//   // }
+// };
 
 export default function page() {
   return (
