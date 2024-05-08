b0ba6d120244e1ffe3de58e38905bc0418a27737
Yash Aggarwal <yaggarwal@EX2INDIA.LOCAL>
2024-05-08 10:24:59 +0530

    loginpage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="bg-white dark:bg-gray-900">
       <div className="flex justify-center h-screen">
         <div
-          className="hidden bg-cover lg:block lg:w-2/3 bg-[url('https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')] bg-cover bg-center"
-          
+          className="hidden bg-cover lg:block lg:w-2/3 bg-[url('https://res.cloudinary.com/dfoa0w717/image/upload/v1715143772/campaign-creators-gMsnXqILjp4-unsplash_atssn5.jpg')] bg-cover bg-center"
+          //https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
         >
           <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
             <div>
