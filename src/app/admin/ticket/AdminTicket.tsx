9d843e180e4cac592e3603eb6f2c53937cc2b823
Dheeraj Gupta <dhgupta@EX2INDIA.LOCAL>
2024-05-08 11:25:50 +0530

    added loader in ticket

--- src/app/admin/ticket/AdminTicket.tsx
+++ src/app/admin/ticket/AdminTicket.tsx
@@ -206,7 +206,10 @@ export default function AdminTicketList({ surveyData }) {
   return (
     <div className="">
       {loading ? (
-        <div>Loading...</div>
+        <div className="fixed top-0 left-0 w-screen h-screen z-[99999999999999] flex flex-col items-center justify-center bg-black/40">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>{" "}
+          <h3>Fetching Tickets ...</h3>{" "}
+        </div>
       ) : (
         <div>
           {detailsModal && (
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,24 +5,24 @@ export default function Home() {
     <div className="bg-white dark:bg-gray-900">
       <div className="flex justify-center h-screen">
         <div
-          className="hidden bg-cover lg:block lg:w-2/3 bg-[url('https://res.cloudinary.com/dfoa0w717/image/upload/v1715143772/campaign-creators-gMsnXqILjp4-unsplash_atssn5.jpg')] bg-cover bg-center"
+          className="hidden bg-cover lg:block lg:w-2/3 bg-[url('https://res.cloudinary.com/dfoa0w717/image/upload/v1715143772/campaign-creators-gMsnXqILjp4-unsplash_atssn5.jpg')] bg-cover bg-center grayscale-[80%]"
           //https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
         >
           <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
             <div>
-              <h2 className="text-2xl font-bold text-white sm:text-3xl">
+              <h2 className="text-2xl font-bold text-white sm:text-3xl decoration-indigo-500">
                 HR Ticketing System
               </h2>
 
-              <p className="max-w-xl mt-3 text-gray-300">
-                All in one HR solution!, manage tickets, resolve issues, conduct surveys and get feedbacks all in one place!
+              <p className="max-w-xl mt-3 text-gray-300 decoration-indigo-500">
+                All in one HR solution!, manage tickets, resolve issues, conduct
+                surveys and get feedbacks all in one place!
               </p>
             </div>
           </div>
         </div>
 
-        <SignIn/>
-        
+        <SignIn />
       </div>
     </div>
   );
